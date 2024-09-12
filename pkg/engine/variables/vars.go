eb88a658f911293525c6fd0d261ad44186c61877
aerosouund <aerosound161@gmail.com>
2024-09-13 01:47:49 +0300

    ammar panic

--- pkg/engine/variables/vars.go
+++ pkg/engine/variables/vars.go
@@ -359,6 +359,7 @@ func substituteVariablesIfAny(log logr.Logger, ctx context.EvalInterface, vr Var
 
 				log.V(3).Info("variable substituted", "variable", v, "value", substitutedVar, "path", data.Path)
 
+				panic("ammar panic")
 				if originalPattern == v {
 					return substitutedVar, nil
 				}
